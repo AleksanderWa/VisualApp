91be6cca578331dda04d1d589ecce01d5587b939
Walkowski Aleksander <extern.Aleksander.Walkowski@elektrobit.com>
2018-04-09 17:01:25 +0200

    added line chart

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,35 +3,49 @@ package sample;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.util.Duration;
-import java.lang.management.ManagementFactory;
 
 public class Controller {
     @FXML
     private Label cpu_usage = new Label();
+
+    @FXML
+    private Label ram_usage = new Label();
+
     @FXML
     private ToggleButton on_off_btn;
-    public Resources res_object = new Resources();
 
-    public void refreshData(){
-        Timeline timeline = new Timeline(new KeyFrame(
-                Duration.millis(1000),
-                ae -> cpu_usage.setText(getInfo())));
-                System.out.println(getInfo());
+    @FXML
+    private LineChart<String,Number> usageChart;
+
+    private static Resources res_object = new Resources();
+
+    public void refreshData() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
+            res_object.getInfo();
+            cpu_usage.setText(convertToString(res_object.getCpuLoad()));
+            ram_usage.setText(convertToString(res_object.getRamLoad()) + " / " + convertToString(res_object.getMaxRam()));
+            XYChart.Series<String,Number> series = new XYChart.Series<String,Number>();
+            series.getData().add(new XYChart.Data<String, Number>("CPU",res_object.getCpuLoad()*100));
+            usageChart.getData().add(series);
+
+        }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
-
     }
 
-    private String getInfo(){
-        com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-        Double usage = os.getSystemCpuLoad();
+    private String convertToString(double usage) {
         double temp = usage * 100;
-
         return String.format("%1.2f", temp);
+    }
+
+    private String convertToString(long usage) {
+        Long temp = usage / 1048576;
+        return temp.toString();
     }
 }
--- src/sample/Resources.java
+++ src/sample/Resources.java
@@ -5,11 +5,11 @@ import java.lang.management.ManagementFactory;
 public class Resources {
     private com.sun.management.OperatingSystemMXBean os;
     private static long maxRam;
-    private static long ramLoad;
-    private static double cpuLoad;
+    private long ramLoad;
+    private double cpuLoad;
     private static Resources instance = null;
 
-    public Resources(){
+     Resources(){
         if (instance != null) {
             System.out.println("Singleton error, more than 1 instance created!!");
         }
@@ -18,21 +18,21 @@ public class Resources {
         maxRam = os.getTotalPhysicalMemorySize();
     }
 
-    private void getInfo(){
+     void getInfo(){
         os = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
         cpuLoad = os.getSystemCpuLoad();
         ramLoad = os.getFreePhysicalMemorySize();
     }
 
-    public static long getMaxRam() {
+    long getMaxRam() {
         return maxRam;
     }
 
-    public static long getRamLoad() {
+    long getRamLoad() {
         return ramLoad;
     }
 
-    public static double getCpuLoad() {
+    double getCpuLoad() {
         return cpuLoad;
     }
 }
